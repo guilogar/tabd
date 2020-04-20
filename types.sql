d209159429dd96d7ec54ed20abe530a54d84c9b6
Guillermo López García <guillermo.lopezgarcia@alum.uca.es>
2020-04-20 03:18:43 +0200

    creation of types

--- types.sql
+++ types.sql
@@ -1,27 +1,77 @@
-create type Address_objtyp as object (
+drop type Address_objtyp force;
+/
+
+drop type Treatment_objtyp force;
+/
+
+drop type TreatmentTypeList_treatmenttyp force;
+/
+
+drop type TreatmentList_ntabtyp force;
+/
+
+drop type Pet_objtyp force;
+/
+
+drop type PetList_pettyp force;
+/
+
+drop type Family_objtyp force;
+/
+
+create or replace type Address_objtyp as object (
     street    varchar(500),
     house     number,
     apartment varchar(500),
     zip       varchar(500)
 );
+/
 
-create type Family_objtyp as object (
+create or replace type TreatmentTypeList_vartyp as varray(500) of varchar(500);
+/
+
+create or replace type Treatment_objtyp as object (
+    id number,
+    treatmentDate date,
+    treatmentType TreatmentTypeList_vartyp
+    -- TODO treatmentType enum('Rabies vaccine',
+    --                   'Vaccination against panleukopenia',
+    --                   'Vaccination against rhinotracheid',
+    --                   'Plague vaccine',
+    --                   'Castration'),
+);
+/
+
+create or replace type TreatmentList_ntabtyp as table of Treatment_objtyp;
+/
+
+create or replace type PetTypeList_vartyp as varray(500) of varchar(500);
+/
+
+create or replace type Pet_objtyp as object (
+    id number,
+    name varchar(500),
+    dateOfBirth date,
+    gender NUMBER(1,0),
+    isTaken NUMBER(1,0),
+    dateOfArrivalShelter date,
+    petType PetTypeList_vartyp,
+    Treatments_List TreatmentList_ntabtyp,
+    dateOfDeparture date
+    -- TODO: Family_obj REF Family_objtyp,
+);
+/
+
+create or replace type PetList_pettyp as varray(500) of Pet_objtyp;
+/
+
+create or replace type Family_objtyp as object (
     id number,
     familyName varchar(500),
     contactName varchar(500),
     contactEmail varchar(500),
-    contactPhone varchar(500),,
-    Address_obj Address_objtyp
-    -- TODO: pets : List<pet>
+    contactPhone varchar(500),
+    Address_obj Address_objtyp,
+    Pets_List PetList_pettyp
 );
-
-create type Pet_objtype as object (
-    id number,
-    name varchar(500),
-    dateOfBirth date,
-    gender boolean,
-    isTaken boolean,
-    dateOfArrivalShelter date,
-    -- TODO petType : PetType
-    
-);
+/
