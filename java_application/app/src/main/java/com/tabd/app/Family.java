d7e6d61ccc70e2d751e76ae5c7be806c4f135cbf
Guillermo López García <lopezgarciaguillermo@live.com>
2020-05-12 06:52:13 +0200

    Herency with Basic Object made. Program works correctly

--- java_application/app/src/main/java/com/tabd/app/Family.java
+++ java_application/app/src/main/java/com/tabd/app/Family.java
@@ -7,56 +7,20 @@ package com.tabd.app;
 
 import java.math.BigDecimal;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Struct;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-public class Family {
-    
-    private Database db;
-    private BigDecimal id;
-    private String table;
-    
+public class Family extends BasicObject
+{
     public Family(Database db, BigDecimal id) throws SQLException
     {
-        this.db = db;
-        this.id = id;
-        this.table = " Family_objtab ";
+        super(db, id, " Family_objtab ");
     }
     
     public boolean familyExists() throws SQLException
     {
-        return !this.getAttributes().isEmpty();
-    }
-    
-    public Map<String, Object> getAttributes() throws SQLException
-    {
-        Map<String, Object> atributos = new LinkedHashMap<>();
-        
-        String[] columns    = { "id" };
-        BigDecimal[] values = { this.id };
-        ResultSet rs = this.db.searchInTableByValue(this.table, columns, values);
-        
-        if(rs.next())
-        {
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int columnCount = rsmd.getColumnCount();
-            for (int i = 1; i <= columnCount; i++ )
-            {
-                String nameAttr = rsmd.getColumnName(i);
-                atributos.put(nameAttr, rs.getObject(nameAttr));
-            }
-        }
-        
-        return atributos;
-    }
-    
-    public boolean update(String procedure, Object newValue) throws SQLException
-    {
-        return this.db.updateInTable(procedure, this.id, newValue) > 0;
+        return this.exists();
     }
     
     public boolean updatePhone(String phone) throws SQLException
@@ -76,65 +40,36 @@ public class Family {
     
     public boolean destroy() throws SQLException
     {
-        String procedure = "deleteFamily";
-        return this.db.destroyInTable(procedure, this.id) > 0;
+        return super.destroy("deleteFamily");
     }
     
     public void printFamily() throws SQLException
     {
-        Map<String, Object> atributos = getAttributes();
-        
-        for(String key : atributos.keySet())
-        {
-            Object value = (Object) atributos.get(key);
-            
-            if(value instanceof Struct)
-            {
-                Struct valueStruct = (Struct) value;
-                Object[] valueAttributes = valueStruct.getAttributes();
-                
-                System.out.println(key + ": ");
-                System.out.println("======================");
-                
-                for(Object attribute : valueAttributes)
-                {
-                    System.out.println(attribute);
-                }
-                System.out.println("======================");
-            } else
-            {
-                System.out.println(key + ": " + value);
-            }
-        }
+        super.print();
     }
     
     public static boolean createFamily(Database db, Object[] values) throws SQLException
     {
-        String procedure = "createFamily";
-        return db.insertInTable(procedure, values) > 0;
+        return BasicObject.create(db, values, "createFamily");
     }
     
-    public static ArrayList<Family> listAllFamilys(Database db) throws SQLException
+    public static ArrayList<BasicObject> listAllFamilys(Database db) throws SQLException
     {
-        ResultSet rset = db.selectByTable("Family_objtab");
-        ArrayList<Family> f = new ArrayList<>();
-        
-        while(rset.next())
-        {
-            Object id = rset.getObject("id");
-            f.add(new Family(db, (BigDecimal) id));
-        }
-        
-        return f;
+        return BasicObject.listAll(db, "Family_objtab");   
     }
     
-    public static ArrayList<Family> searchFamilys(Database db, String familyName, boolean useLike) throws SQLException
+    public static void printFamilys(ArrayList<BasicObject> familys) throws SQLException
+    {
+        BasicObject.print(familys);
+    }
+    
+    public static ArrayList<BasicObject> searchFamilys(Database db, String familyName, boolean useLike) throws SQLException
     {
         String[] columns = { " lower(familyname) like " };
         Object[] values  = { (useLike) ? "%" + familyName + "%" : familyName };
         ResultSet rset = db.searchInTableByValue("Family_objtab", columns, values);
         
-        ArrayList<Family> f = new ArrayList<>();
+        ArrayList<BasicObject> f = new ArrayList<>();
             
         while(rset.next())
         {
@@ -143,15 +78,5 @@ public class Family {
         }
         
         return f;
-    }
-    
-    public static void printFamilys(ArrayList<Family> familys) throws SQLException
-    {
-        for(Family f : familys)
-        {
-            f.printFamily();
-            System.out.println("=======================================");
-            System.out.println("=======================================");
-        }
     }
 }
--- java_application/app/src/main/java/com/tabd/app/Pet.java
+++ java_application/app/src/main/java/com/tabd/app/Pet.java
@@ -10,54 +10,19 @@ import java.sql.Array;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Struct;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class Pet {
-    
-    private Database db;
-    private BigDecimal id;
-    private String table;
-    
+public class Pet extends BasicObject
+{
     public Pet(Database db, BigDecimal id) throws SQLException
     {
-        this.db = db;
-        this.id = id;
-        this.table = " Pet_objtab ";
+        super(db, id, " Pet_objtab ");
     }
     
     public boolean petExists() throws SQLException
     {
-        return !this.getAttributes().isEmpty();
-    }
-    
-    public Map<String, Object> getAttributes() throws SQLException
-    {
-        Map<String, Object> atributos = new LinkedHashMap<>();
-        
-        String[] columns    = { "id" };
-        BigDecimal[] values = { this.id };
-        ResultSet rs = this.db.searchInTableByValue(this.table, columns, values);
-        
-        if(rs.next())
-        {
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int columnCount = rsmd.getColumnCount();
-            for (int i = 1; i <= columnCount; i++ )
-            {
-                String nameAttr = rsmd.getColumnName(i);
-                atributos.put(nameAttr, rs.getObject(nameAttr));
-            }
-        }
-        
-        return atributos;
-    }
-    
-    public boolean update(String procedure, Object newValue) throws SQLException
-    {
-        return this.db.updateInTable(procedure, this.id, newValue) > 0;
+        return this.exists();
     }
     
     public boolean updateDateBirth(String date) throws SQLException
@@ -82,12 +47,12 @@ public class Pet {
     
     public boolean deleteFamily() throws SQLException
     {
-        return this.db.destroyInTable("cancelAdoption", this.id) > 0;
+        return super.destroy("cancelAdoption");
     }
     
     public boolean destroy() throws SQLException
     {
-        return this.db.destroyInTable("deletePet", this.id) > 0;
+        return super.destroy("deleteFamily");
     }
     
     public void printTreatmentList() throws SQLException
@@ -116,69 +81,22 @@ public class Pet {
     
     public void printPet() throws SQLException
     {
-        Map<String, Object> atributos = getAttributes();
-        
-        for(String key : atributos.keySet())
-        {
-            Object value = (Object) atributos.get(key);
-            
-            if(value instanceof Struct)
-            {
-                Struct valueStruct = (Struct) value;
-                Object[] valueAttributes = valueStruct.getAttributes();
-                
-                System.out.println(key + ": ");
-                System.out.println("======================");
-                
-                for(Object attribute : valueAttributes)
-                {
-                    System.out.println(attribute);
-                }
-                System.out.println("======================");
-            } else if(value instanceof Array)
-            {
-                Array valueArray = (Array) value;
-                ResultSet rs = valueArray.getResultSet();
-                
-                System.out.println(key + ": ");
-                System.out.println("======================");
-                
-                while(rs.next())
-                {
-                    ResultSetMetaData rsmd = rs.getMetaData();
-                    int columnCount = rsmd.getColumnCount();
-                    
-                    for(int i = 0; i < columnCount; i++)
-                    {
-                        System.out.println(rs.getObject(i));
-                    }
-                }
-                
-                System.out.println("======================");
-            } else
-            {
-                System.out.println(key + ": " + value);
-            }
-        }
+        super.print();
     }
     
     public static boolean createPet(Database db, Object[] values) throws SQLException
     {
-        return db.insertInTable("createPet", values) > 0;
+        return BasicObject.create(db, values, "createPet");
     }
     
-    public static ArrayList<Pet> listAllPets(Database db) throws SQLException
+    public static ArrayList<BasicObject> listAllPets(Database db) throws SQLException
     {
-        ResultSet rset = db.selectByTable("Pet_objtab");
-        ArrayList<Pet> p = new ArrayList<>();
-        
-        while(rset.next())
-        {
-            Object id = rset.getObject("id");
-            p.add(new Pet(db, (BigDecimal) id));
-        }
-        
-        return p;
+        return BasicObject.listAll(db, "Pet_objtab");   
+    }
+    
+    public static void printPets(ArrayList<BasicObject> pets) throws SQLException
+    {
+        BasicObject.print(pets);
     }
     
     public static ArrayList<Pet> searchPets(Database db, String name, boolean useLike) throws SQLException
@@ -196,15 +114,5 @@ public class Pet {
         }
         
         return p;
-    }
-    
-    public static void printPets(ArrayList<Pet> pets) throws SQLException
-    {
-        for(Pet p : pets)
-        {
-            p.printPet();
-            System.out.println("=======================================");
-            System.out.println("=======================================");
-        }
     }
 }
